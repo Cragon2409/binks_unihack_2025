84b2f407699da7c756859e7946735284227d4671
TheSamuel <75915760+Cragon2409@users.noreply.github.com>
2025-03-16 09:11:34 +1100

    Dev (#35)
    * Removing duplicate files. Using .ts for regular files
    
    * Timetable pulling course and assessment data
    
    * Feature/dashboard (#34)
    
    * added basic layout
    
    * Implemented basic dashboard functions
    
    ---------
    
    Co-authored-by: Samuel Price <u7482502@anu.edu.au>
    
    ---------
    
    Co-authored-by: hkattt <hugo.kat3@gmail.com>
    Co-authored-by: jsmnzhou <jsmnzhou@gmail.com>
    Co-authored-by: Hugo Kat <54300585+hkattt@users.noreply.github.com>
    Co-authored-by: Samuel Price <u7482502@anu.edu.au>

--- src/API/supabase.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://quaobmjerksaujqlspoz.supabase.co';
-const VITE_ANON_KEY = process.env.VITE_ANON_KEY ?? "";
-
-export const supabase = createClient(SUPABASE_URL, VITE_ANON_KEY);
