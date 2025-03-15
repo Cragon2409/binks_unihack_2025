1230fcdb43f1eb7fbacf12d929cd72c4139352e6
TheSamuel <75915760+Cragon2409@users.noreply.github.com>
2025-03-15 17:39:57 +1100

    Feature/auth03 (#17)
    * implemented discord log in
    
    * made feature branch up to date
    
    * Added sign out button and username display to footer
    
    * updated commands for running guide
    
    ---------
    
    Co-authored-by: Samuel Price <u7482502@anu.edu.au>

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ function App() {
   },[courseTable])
 
   if (!session) { //display log in page if not logged in
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={['discord','github']} />)
   } else {
     return (
       <ThemeProvider>
