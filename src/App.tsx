51c8db4cdaf7b83fa9662cd21763f710b2ab213e
Samuel Price <u7482502@anu.edu.au>
2025-03-15 11:50:56 +1100

    made auth work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createClient } from '@supabase/supabase-js'
+// import { Database } from './database.types'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const SUPABASE_URL = 'https://quaobmjerksaujqlspoz.supabase.co'
+const ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1YW9ibWplcmtzYXVqcWxzcG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5OTcxNjYsImV4cCI6MjA1NzU3MzE2Nn0._8mMNm-VNhhMNdkQVsu906Pgs7UWq1_c5X0NqLsPYYA'
+
+const supabase = createClient(SUPABASE_URL, ANON_KEY)
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session as React.SetStateAction<null>)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session as React.SetStateAction<null>)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
 
   return (
     <>
