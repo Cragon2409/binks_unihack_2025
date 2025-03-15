5a8e2dcf36885c1856bbafc214febe38bc1ce2e6
jsmnzhou <jsmnzhou@gmail.com>
2025-03-15 19:39:37 +1100

    fix login page issue

--- src/API/store.tsx
+++ src/API/store.tsx
@@ -2,11 +2,20 @@ import { configureStore } from '@reduxjs/toolkit'
 import courseReducer from './coursesSlice'
 import sessionReducer from './sessionSlice'
 import assessmentReducer from './assessmentsSlice'
+import storage from 'redux-persist/lib/storage'; 
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+const persistedSessionReducer = persistReducer(persistConfig, sessionReducer);
 
 export const store = configureStore({
   reducer: {
     courses: courseReducer,
-    session: sessionReducer,
+    session: persistedSessionReducer,
     assessments: assessmentReducer
   }
 })
@@ -14,3 +23,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export type AppStore = typeof store
+
+export const persistor = persistStore(store);
