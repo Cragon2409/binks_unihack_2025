ac98ca76883522b651fb75ab82e1aca42b177e79
Samuel Price <u7482502@anu.edu.au>
2025-03-15 21:32:49 +1100

    corrected merge resolution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { Routes, Route } from 'react-router-dom';
 
 import { supabase } from './API/supabase';
 
 import AppLayout from './components/Layout/AppLayout'
-import LoginLayout from './components/Layout/LoginLayout'
+// import LoginLayout from './components/Layout/LoginLayout'
 import ThemeProvider from './components/ThemeProvider/ThemeProvider'
 import Dashboard from './pages/Dashboard/Dashboard';
 import Courses from './pages/Courses/Courses';
 import Timetable from './pages/Timetable/Timetable';
 import Login from './pages/Login/Login';
+import LoginLayout from './components/Layout/LoginLayout'
 
 import { useAppDispatch, useAppSelector } from './API/hooks'
 import { fetchCourses } from './API/coursesSlice'
@@ -25,7 +26,7 @@ function App() {
 
   const dispatch = useAppDispatch();
   // const [_errorLogMessage, setErrorLogMessage] = useState("")
-  const [courseTable, _setCourseTable] = useState(null)
+  // const [courseTable, _setCourseTable] = useState(null)
 
   console.log(courses)
 
@@ -49,34 +50,32 @@ function App() {
     if (session != null) {
       dispatch(fetchCourses((session as any)?.user.id));
 
-      setLoading(false);
+      // setLoading(false);
     }
   }, [session]);
 
-  if (loading) {
-    return (
-      <ThemeProvider>
-        <main className="loading-screen-main">
-            loading...
-        </main>
-      </ThemeProvider>
-    )
-  } else if (!session) { //display log in page if not logged in
-    return (
-      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={['discord','github']} />)
-  } else {
-    return (
-      <ThemeProvider>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="courses" element={<Courses />} />
-            <Route path="timetable" element={<Timetable />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
-    );  
-  }
+  return (
+    <ThemeProvider>
+      <Routes>
+        {
+          session ? 
+          (
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="courses" element={<Courses />} />
+              <Route path="timetable" element={<Timetable />} />
+            </Route>
+          )
+          :
+          (
+            <Route path="/*" element={<LoginLayout />}>
+              <Route path="*" element={<Login supabase={supabase} />} />
+            </Route>
+          )
+        }
+      </Routes>
+    </ThemeProvider>
+  )
 
 }
 
