4add1bc4738c63b0472e8af37042d6d804c22a75
Samuel Price <u7482502@anu.edu.au>
2025-03-15 21:27:30 +1100

    added basic layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,40 @@
 import { Typography } from 'antd';
+import { fetchAssessments } from '../../API/assessmentsSlice';
+import { useAppDispatch, useAppSelector } from '../../API/hooks'
+import { useEffect } from 'react'
 
 const { Title } = Typography;
 
 export default function Dashboard() {
+  const session = useAppSelector(( state ) => state.session.session)
+  const assessments = useAppSelector(( state) => state.assessments.assessments);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+      dispatch(fetchAssessments((session as any)?.user.id));
+  }, [session]);
+
+  useEffect(() => {
+    console.log(assessments)
+  }, [assessments])
+
   return (
-    <Title>Dashboard</Title>
+    <main className="dashboard-page">
+      <Title>Dashboard</Title>
+
+      <div className="main-content">
+        <div className="large">Large Item</div>
+        <div className="grid">
+          <div className="small">Item 1</div>
+          <div className="small">Item 2</div>
+          <div className="small">Item 3</div>
+          <div className="small">Item 4</div>
+          <div className="small">Item 5</div>
+          <div className="small">Item 6</div>
+        </div>
+      </div>
+    </main>
+    
+
   );
 }
