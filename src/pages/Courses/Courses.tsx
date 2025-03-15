29c269258b851a42efd369edf9fbb1122ae5e35d
David C <cooladave@gmail.com>
2025-03-15 14:44:28 +1100

    Initial foundation

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -1,9 +1,34 @@
-import { Typography } from 'antd';
+import { useState } from "react";
+import { Typography, Button, Flex } from 'antd';
+
 
 const { Title } = Typography;
 
+const courseButtonStyle: React.CSSProperties = {
+  display: "flex",
+  alignItems: "center",
+};
+
 export default function Courses() {
+  const [courses, setCourses] = useState<string[]>([]);
+  const addCourse = () => {
+    setCourses([...courses, "Course Name"]);
+  };
+
   return (
-    <Title>Courses</Title>
+    <>
+      <Flex gap="small" align="center">
+        <Title>Courses</Title>
+        <Button onClick={addCourse}> Add Course </Button>
+      </Flex>
+      <div className="course-container">
+        <Flex wrap gap="small">
+          {courses.map((course, index) => (
+            <Button style={courseButtonStyle} key={index}> {course} </Button>
+          ))}
+            <Button style={courseButtonStyle} > Woah a Course </Button>
+        </Flex>
+      </div>
+    </>
   );
 }
