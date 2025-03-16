fd366e4d6d2206b60d7f8a7e649a5942944c08d2
hkattt <hugo.kat3@gmail.com>
2025-03-16 11:30:17 +1100

    Moved account management to header

--- src/components/Layout/HeaderContent.tsx
+++ src/components/Layout/HeaderContent.tsx
@@ -1,17 +1,35 @@
-import { Flex, Typography, Menu } from 'antd';
+import { useState, useEffect } from 'react';
+import { 
+  Flex, 
+  Typography, 
+  Menu, 
+  Button,
+  Modal
+} from 'antd';
 import type { MenuProps } from 'antd';
-// import { useAppSelector } from '../../API/hooks';
+import { UserOutlined } from '@ant-design/icons';
 
+import { supabase } from '../../API/supabase';
+
+import { useAppSelector } from '../../API/hooks';
 import * as Constants from '../../common/Constants'
 
 
-const { Link } = Typography;
+const { Text, Link } = Typography;
 
 type MenuItem = Required<MenuProps>['items'][number];
 
 const items: MenuItem[] = [
-  { 
+  {
     key: 1, 
+    label: (
+      <Link href="/" rel="noopener noreferrer">
+        Dashboard
+      </Link>
+    )
+  },
+  { 
+    key: 2, 
     label: (
       <Link href="/courses" rel="noopener noreferrer">
         Courses
@@ -19,44 +37,105 @@ const items: MenuItem[] = [
     ) 
   },
   { 
-    key: 2, 
+    key: 3, 
     label: (
       <Link href="/timetable" rel="noopener noreferrer">
         Timetable
       </Link>
-    )
-  },
-  {
-  key: 3, 
-  label: (
-    <Link href="/" rel="noopener noreferrer">
-      Dashboard
-    </Link>
-  )
+    ),
   }
 ];
 
 export default function HeaderContent() {
-  // const session = useAppSelector(( state : any ) => state.session.session)
+  const session = useAppSelector(( state : any ) => state.session.session)
+  const [selectedKey, setSelectedKey] = useState<number>(
+    Number(localStorage.getItem('selectedKey')) || 1
+  );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('selectedKey', selectedKey.toString())
+  }, [selectedKey]);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Flex 
       style={{
         maxWidth: Constants.maxWidth,
         width: "100%",
         margin: "0 auto",
+        padding: "12px"
       }} 
       vertical={false} 
       align="center" 
       justify="space-between"
     >
-      <Link style={{ fontSize: "20px", lineHeight: "28px" }} href="/" rel="noopener noreferrer" strong>
+      <Link 
+        style={{ 
+          fontSize: "20px", 
+          lineHeight: "28px",
+          whiteSpace: 'nowrap'
+        }} 
+        href="/" 
+        rel="noopener noreferrer" 
+        onClick={() => setSelectedKey(1)}
+        strong
+        
+      >
         Unitrack.
       </Link>
-      <Menu
-        style={{width : "25%"}}
-        mode="horizontal"
-        items={items}
-      />
+      <Flex 
+        style={{
+          width: "400px"
+        }}
+        gap="small" 
+        justify='flex-end' 
+        align='center'
+      >
+        <Menu
+          style={{
+            width: "70%"
+          }}
+          mode="horizontal"
+          items={items}
+          selectedKeys={[selectedKey.toString()]}
+          onClick={(e) => setSelectedKey(Number(e.key))}
+          disabledOverflow={false}
+        />
+        <Button 
+          icon={<UserOutlined />} 
+          onClick={showModal}
+        />
+      </Flex>
+      <Modal 
+        title="Profile" 
+        open={isModalOpen} 
+        onOk={handleOk} 
+        onCancel={handleCancel}
+        footer={[
+          <Button 
+            key="submit" 
+            type="primary" 
+            onClick={() => supabase.auth.signOut()}>
+            Logout
+          </Button>,
+        ]}
+      >
+        <Text>
+          {session ? `You are logged in with ${(session as any).user.email}` : ""}
+        </Text>
+      </Modal>
     </Flex>
   );
 }
