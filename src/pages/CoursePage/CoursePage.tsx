80d278f4770857bf8595d13ecd21e1d44b9a64dc
hkattt <hugo.kat3@gmail.com>
2025-03-27 00:16:58 +1100

    course page shows name

--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import {
   Flex,
-  Typography
+  Typography,
+  Spin
 } from 'antd';
+
+import { useAppDispatch, useAppSelector } from '../../API/hooks'
+import { fetchCourses } from '../../API/coursesSlice'
+
+import { Course } from '../../common/Types';
 
 const { Title } = Typography;
 
 const CoursePage = () => {
+  // Extract courseId from the URL
+  const { courseId } = useParams(); 
+  const [ course, setCourse ] = useState<Course | null>(null);
+  const courses = useAppSelector(( state ) => state.courses.courses);
+  const session = useAppSelector(( state ) => state.session.session);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCourses((session as any)?.user.id));
+  }, [session]);
+
+  useEffect(() => {
+    if (courses.length > 0 && courseId) {
+      setCourse(
+        courses.find((course) => course.id === Number(courseId)) || null
+      );
+    }
+  })
+
   return (
     <Flex vertical>
-      <Title>Course</Title>
+      {
+        course ? (
+          <Title>{course.name}</Title>
+        ) :
+        <Spin />
+      }
+      
     </Flex>
   )
 }
