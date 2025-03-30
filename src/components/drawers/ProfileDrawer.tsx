6b244e1db1c7b9f34d01d5e6eaa3c5f70cc3f113
hkattt <hugo.kat3@gmail.com>
2025-03-30 18:22:22 +1100

    Moved the theme toggle

--- src/components/drawers/ProfileDrawer.tsx
+++ src/components/drawers/ProfileDrawer.tsx
@@ -3,12 +3,19 @@ import React from 'react'
 import {
   Drawer,
   Flex,
+  Space,
   Button,
   Typography
 } from 'antd';
 
+import {
+  MoonOutlined,
+  SunOutlined
+} from '@ant-design/icons';
+
 const { Text } = Typography;
 
+import { useTheme } from '../ThemeProvider/ThemeProvider';
 import { useAppSelector } from '../../API/hooks';
 import { supabase } from '../../API/supabase';
 
@@ -18,7 +25,8 @@ interface ProfileDrawerProps {
 }
 
 const ProfileDrawer = ({ isOpen, setIsOpen } : ProfileDrawerProps) => {
-  const session = useAppSelector(( state : any ) => state.session.session)
+  const session = useAppSelector(( state : any ) => state.session.session);
+  const { isDark, toggleTheme } = useTheme();
 
   return (
     <Drawer
@@ -30,8 +38,31 @@ const ProfileDrawer = ({ isOpen, setIsOpen } : ProfileDrawerProps) => {
         <Text>
           {`You are logged in with ${(session as any).user.email}`}
         </Text>
+        <Space.Compact 
+          block
+        >
+          <Button 
+            style={{
+              width: '100%'
+            }}
+            type={isDark ? 'default' : 'primary'}
+            icon={<SunOutlined />} 
+            onClick={toggleTheme}
+          >
+            Light
+          </Button>
+          <Button 
+            style={{
+              width: '100%'
+            }}
+            type={isDark ? 'primary' : 'default'}
+            icon={<MoonOutlined />}
+            onClick={(toggleTheme)}
+          >
+            Dark
+          </Button>
+        </Space.Compact>
         <Button 
-          key="submit" 
           danger
           onClick={() => supabase.auth.signOut()}>
           Logout
